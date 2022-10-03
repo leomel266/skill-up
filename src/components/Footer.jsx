c9fd4fa84305ffeb995592ea9887d3cd25086abd
Leonardo Melchiori <91997494+leomel266@users.noreply.github.com>
2022-10-03 12:39:08 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className='text-center'>
-            <p>Alkemy challenge</p>
-        </footer>
-    );
-}
+  return (
+    <footer className='footer text-center' style={{ fontWeight: "bold" }}>
+      <p>Alkemy challenge</p>
+    </footer>
+  );
+};
 
 export default Footer;
