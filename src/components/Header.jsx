9159575ba538bbd9e1981e084c4242e6ffae3072
Leonardo Melchiori <91997494+leomel266@users.noreply.github.com>
2022-10-11 17:58:42 -0300

    classname

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,37 +5,26 @@ import Buscador from "./Buscador";
 const Header = () => {
   return (
     <header>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
+      <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+        <div className='container-fluid'>
+          <Link className='navbar-brand' to='/'>
             MFlix
           </Link>
           <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
+            className='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarNav'
+            aria-controls='navbarNav'
+            aria-expanded='false'
+            aria-label='Toggle navigation'>
+            <span className='navbar-toggler-icon'></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
+          <div className='collapse navbar-collapse' id='navbarNav'>
+            <ul className='navbar-nav'>
+              <li className='nav-item'>
+                <Link className='nav-link' to='/'>
                   Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/listado">
-                  Listado
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/favoritos">
-                  Favoritos
                 </Link>
               </li>
             </ul>
